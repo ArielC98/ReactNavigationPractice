66a5a44012e15ed7a60f3595b26d1ea5adf82e88
Ariel <ariel.calderon@epn.edu.ec>
2025-05-18 17:02:20 -0500

    Products list added

--- src/routes/StackNavigator.tsx
+++ src/routes/StackNavigator.tsx
@@ -2,14 +2,24 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../presentation/screens/home/HomeScreen';
 import { ProductsScreen } from '../presentation/screens/products/ProductsScreen';
 import { SettingsScreen } from '../presentation/screens/settings/SettingsScreen';
+import { ProductScreen } from '../presentation/screens/products/ProductScreen';
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerStyle:{
+          elevation:0,
+          shadowColor: 'transparent'
+        }
+      }}
+    >
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Products" component={ProductsScreen} />
+      <Stack.Screen name="Product" component={ProductScreen} />
       <Stack.Screen name="Settings" component={SettingsScreen} />
     </Stack.Navigator>
   );
