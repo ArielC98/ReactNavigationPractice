ce1a0a4977e4de91334b2f808ac54551ff2b1107
ArielC98 <arielcalderon100@gmail.com>
2025-05-19 00:40:24 -0500

    bugs fixed

--- src/routes/DrawerNavigator.tsx
+++ src/routes/DrawerNavigator.tsx
@@ -1,14 +1,51 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentComponentProps, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { StackNavigator } from './StackNavigator';
 import { ProfileScreen } from '../presentation/screens/profile/ProfileScreen';
+import { useWindowDimensions, View } from 'react-native';
+import { globalColors } from '../presentation/theme/theme';
 
 const Drawer = createDrawerNavigator();
 
 export const DrawerNavigator = () => {
+
+
+  const dimensions = useWindowDimensions();
+
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator    
+      drawerContent = {(props) => <CustomDrawerContent {...props}/>}
+      screenOptions={{
+        drawerType: (dimensions.width >= 760)?'permanent':'slide',
+
+        headerShown:false,
+        drawerActiveBackgroundColor: globalColors.primary,
+        drawerActiveTintColor: 'white',
+        drawerInactiveTintColor: globalColors.primary,
+        drawerItemStyle: {
+          borderRadius: 100,
+          paddingHorizontal: 20
+        }
+      }}
+    >
       <Drawer.Screen name="StackNavigator" component={StackNavigator} />
       <Drawer.Screen name="Profile" component={ProfileScreen} />
-    </Drawer.Navigator>
+    </Drawer.Navigator >
   );
+}
+
+const CustomDrawerContent = (props: DrawerContentComponentProps) =>{
+  return(
+    <DrawerContentScrollView>
+      <View
+        style= {{
+          height:150,
+          backgroundColor:globalColors.primary,
+          margin:30,
+          borderRadius:50
+        }}
+      />
+
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  )
 }
