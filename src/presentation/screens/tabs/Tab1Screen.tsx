b4dec8657a219a52020bc46465ae234e251e1dae
Ariel <ariel.calderon@epn.edu.ec>
2025-05-23 13:57:59 -0500

    icon component added

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,17 +1,16 @@
-import { DrawerActions, useNavigation } from "@react-navigation/native"
-import { useEffect } from "react";
-import { Pressable, Text, View } from "react-native";
+
+import {  Text, View } from "react-native";
 import Icon from 'react-native-vector-icons/Ionicons'
 
 
 export const Tab1Screen = () => {
 
-  const navigation = useNavigation();
-
 
   return (
     <View>
       <Text>Tab1Screen</Text>
+
+      <Icon name="rocket-outline" size={30} color="#900"/>
     </View>
   )
 }
