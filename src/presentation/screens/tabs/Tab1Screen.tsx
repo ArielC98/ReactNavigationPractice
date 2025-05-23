439920725e1c98879f217e20ad6b0544c260909b
Ariel <ariel.calderon@epn.edu.ec>
2025-05-23 13:54:47 -0500

    ionicons installed

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,6 +1,7 @@
 import { DrawerActions, useNavigation } from "@react-navigation/native"
 import { useEffect } from "react";
-import { Pressable, Text, View } from "react-native"
+import { Pressable, Text, View } from "react-native";
+import Icon from 'react-native-vector-icons/Ionicons'
 
 
 export const Tab1Screen = () => {
